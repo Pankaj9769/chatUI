3e49a2008d4d145945818eb21345a4d260857a41
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-12-22 13:39:39 +0530

    Modified VideoCall.jsx

--- src/Pages/VideoCall.jsx
+++ src/Pages/VideoCall.jsx
@@ -116,7 +116,7 @@ const VideoCallDialog = ({ closeCall, socket }) => {
 
           // Check and assign the video element
           const remoteVideo = document.getElementById("remote-video");
-          console.log(remoteVideo);
+          console.log(remoteVideo + "->" + remoteStreamRef.current);
           if (remoteVideo) {
             remoteVideo.srcObject = remoteStreamRef.current;
             console.log("Assigned remoteStreamRef to remote-video.");
