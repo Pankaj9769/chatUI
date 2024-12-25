da2dd3bb884665e55e9498d92acae556fe55ee7c
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-12-26 00:30:07 +0530

    Implemented VideoCalling

--- src/services/peer.js
+++ src/services/peer.js
@@ -4,9 +4,16 @@ class PeerService {
     if (!this.peer) {
       this.peer = new RTCPeerConnection({
         iceServers: [
-          {
-            urls: "stun:stun.relay.metered.ca:80",
-          },
+          { urls: "stun:stun.l.google.com:19302" },
+          { urls: "stun:stun.l.google.com:5349" },
+          { urls: "stun:stun1.l.google.com:3478" },
+          { urls: "stun:stun1.l.google.com:5349" },
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun2.l.google.com:5349" },
+          { urls: "stun:stun3.l.google.com:3478" },
+          { urls: "stun:stun3.l.google.com:5349" },
+          { urls: "stun:stun4.l.google.com:19302" },
+          { urls: "stun:stun4.l.google.com:5349" },
           {
             urls: "turn:in.relay.metered.ca:80",
             username: "98bcd1a1beadc91ee56f60bc",
