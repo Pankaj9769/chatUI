5c8f47c72413dd4a6f189a1477a748d1f067c677
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-12-26 19:14:58 +0530

    Implemented VideoCalling

--- src/services/peer.js
+++ src/services/peer.js
@@ -87,25 +87,14 @@ class PeerService {
     if (!this.peer) {
       this.peer = new RTCPeerConnection({
         iceServers: [
-          { urls: "stun:stun.relay.metered.ca:80" },
-          { urls: "stun:stun.voxgratia.org" }, // STUN server as fallback
           {
-            urls: "turn:in.relay.metered.ca:80",
-            username: "98bcd1a1beadc91ee56f60bc",
-            credential: "s25G1WH5IOc+bxCA",
-          },
-          {
-            urls: "turn:in.relay.metered.ca:80?transport=tcp",
-            username: "98bcd1a1beadc91ee56f60bc",
-            credential: "s25G1WH5IOc+bxCA",
+            urls: [
+              "stun:stun.l.google.com:19302",
+              "stun:stun1.l.google.com:19302",
+            ],
           },
           {
             urls: "turn:in.relay.metered.ca:443",
-            username: "98bcd1a1beadc91ee56f60bc",
-            credential: "s25G1WH5IOc+bxCA",
-          },
-          {
-            urls: "turns:in.relay.metered.ca:443?transport=tcp",
             username: "98bcd1a1beadc91ee56f60bc",
             credential: "s25G1WH5IOc+bxCA",
           },
