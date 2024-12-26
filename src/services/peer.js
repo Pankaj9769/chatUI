22111eae4c0ccb848d357ad977446c1ef84899a4
Pankaj Parihar <pnkjparihar38@gmail.com>
2024-12-26 19:06:01 +0530

    Implemented VideoCalling

--- src/services/peer.js
+++ src/services/peer.js
@@ -87,17 +87,28 @@ class PeerService {
     if (!this.peer) {
       this.peer = new RTCPeerConnection({
         iceServers: [
+          { urls: "stun:stun.relay.metered.ca:80" },
+          { urls: "stun:stun.voxgratia.org" }, // STUN server as fallback
           {
             urls: "turn:in.relay.metered.ca:80",
             username: "98bcd1a1beadc91ee56f60bc",
             credential: "s25G1WH5IOc+bxCA",
           },
           {
-            urls: "turns:in.relay.metered.ca:443",
+            urls: "turn:in.relay.metered.ca:80?transport=tcp",
             username: "98bcd1a1beadc91ee56f60bc",
             credential: "s25G1WH5IOc+bxCA",
           },
-          { urls: "stun:stun.voxgratia.org" }, // STUN server as fallback
+          {
+            urls: "turn:in.relay.metered.ca:443",
+            username: "98bcd1a1beadc91ee56f60bc",
+            credential: "s25G1WH5IOc+bxCA",
+          },
+          {
+            urls: "turns:in.relay.metered.ca:443?transport=tcp",
+            username: "98bcd1a1beadc91ee56f60bc",
+            credential: "s25G1WH5IOc+bxCA",
+          },
         ],
         iceTransportPolicy: "relay",
       });
@@ -117,6 +128,10 @@ class PeerService {
           console.log("ICE gathering complete");
         }
       };
+
+      this.socket.on("ice-candidate", (candidate) => {
+        this.peer.addIceCandidate(candidate);
+      });
 
       this.peer.oniceconnectionstatechange = (event) => {
         console.log(
